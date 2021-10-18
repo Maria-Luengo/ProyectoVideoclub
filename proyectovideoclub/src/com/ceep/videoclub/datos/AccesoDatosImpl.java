540d6ebfb5ba7aae91a9887ed2914b83f6b8cac0
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-10-18 12:11:49 +0200

    Create AccesoDatosImpl.java

---
+++ proyectovideoclub/src/com/ceep/videoclub/datos/AccesoDatosImpl.java
@@ -0,0 +1,31 @@
+
+package com.ceep.videoclub.datos;
+
+
+public class AccesoDatosImpl {
+    
+    //metodos EXISTE (String): boolean y LISTAR
+    
+    public boolean existe(String nombreArchivo){
+        var a=0;
+        return true;
+    }
+    
+    public String listar(String nombreArchivo){
+        //array List películar
+        return nombreArchivo;
+    }
+    
+    public void escribir(boolean anexar){
+        
+    }
+    
+    public String crear(String nombreArchivo){
+        //
+        return nombreArchivo;
+    }
+    public String buscar(String nombreArchivo){
+        //
+        return nombreArchivo;
+    }
+}
