b26043c49eba76cdb24a145943a62b057206ccb5
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-10-19 12:04:49 +0200

    Create ExEscritura.java

---
+++ proyectovideoclub/src/com/ceep/videoclub/excepciones/ExEscritura.java
@@ -0,0 +1,13 @@
+
+package com.ceep.videoclub.excepciones;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class ExEscritura extends AccesoDatosExcepciones{
+    
+    public ExEscritura(String mensaje) {
+        super(mensaje);
+    }
+}
