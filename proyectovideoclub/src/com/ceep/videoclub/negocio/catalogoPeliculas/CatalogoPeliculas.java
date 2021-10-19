79031e50b4808036a13f29b0bb2b72cee1e84655
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-10-19 12:04:44 +0200

    Create CatalogoPeliculas.java

---
+++ proyectovideoclub/src/com/ceep/videoclub/negocio/catalogoPeliculas/CatalogoPeliculas.java
@@ -0,0 +1,31 @@
+
+package com.ceep.videoclub.negocio.catalogoPeliculas;
+
+import com.ceep.videoclub.dominio.Pelicula;
+import java.util.*;
+
+
+public class CatalogoPeliculas implements ICatalogoPelicula{
+
+    @Override
+    public void agregarPelícula(String nombrePelicula, String nombreArchivo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void listarPelículas(String nombreArchivo) {
+        List<Pelicula> peliculas = new ArrayList<>();
+        peliculas = listar(nombreArchivo);
+    }
+
+    @Override
+    public void buscarPelicula(String nombreArchivo, String buscar) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void iniciarArchivo(String nombreArchivo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
