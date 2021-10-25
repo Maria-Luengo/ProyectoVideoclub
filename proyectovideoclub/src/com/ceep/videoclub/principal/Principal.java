5642b6825d6eedeffb8e7939ae5b856258b254a7
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-10-25 12:13:11 +0200

    d
    d

--- proyectovideoclub/src/com/ceep/videoclub/principal/Principal.java
+++ proyectovideoclub/src/com/ceep/videoclub/principal/Principal.java
@@ -5,14 +5,54 @@ import com.ceep.videoclub.negocio.CatalogoPeliculasImp;
 import com.ceep.videoclub.negocio.ICatalogoPelicula;
 import java.util.Scanner;
 
-public class Test {
+public class Principal {
 
     public static void main(String[] args) {
         //Creamos puente de comuniucacion con Catalogo
         var nombreCatalogo = "Videoclub.txt";
         ICatalogoPelicula catalogo = new CatalogoPeliculasImp();
-      
-        while (true) {
+        //catalogo.agregarPelicula("Error 404", nombreCatalogo);
+
+        var nombrePeli="";
+        var opcion = -1;
+        var lectura = new Scanner(System.in);
+        while (opcion != 0) {
+            System.out.println("Elige una opción: \n"
+                    + "1.- Iniciar cartalgogo\n"
+                    + "2.- Agregar pelicula\n"
+                    + "3.- Listar peliculas\n"
+                    + "4.- Buscar pelicula\n"
+                    + "0.- Salir");
+            opcion = Integer.parseInt(lectura.nextLine());
+
+            switch (opcion) {
+                case 1:
+                    catalogo.iniciarCatalogo(nombreCatalogo);
+                    System.out.println("Catalogo iniciado...");
+                    break;
+                case 2:
+                    System.out.print("Introduce el nombre de la película a agregar: \t");
+                    nombrePeli = lectura.nextLine();
+                    catalogo.agregarPelicula(nombrePeli, nombreCatalogo);
+                    System.out.println("Se ha agegado \"" + nombrePeli + "\" al catalogo");
+                    break;
+                case 3:
+                    catalogo.listarPeliculas(nombreCatalogo);
+                    break;
+                case 4:
+                    System.out.print("Introduce nombre de película que quieres buscar:\t");
+                    nombrePeli=lectura.nextLine();
+                    catalogo.buscarPelicula(nombreCatalogo, nombrePeli);
+                    break;
+                case 0:
+                    System.out.println("Adiós y hasta la printo!!");
+                    break;
+                default:
+                    System.out.println("Opción desconocida");
+            }
+        }
+
+        /*while (true) {
             System.out.println("MENU");
             System.out.println(" 1.- Catalogo peliculas");
             System.out.println(" 2.- Agregar pelicula");
@@ -46,8 +86,7 @@ public class Test {
                     System.out.println("Gracias!, hasta la proxima");
                     break;
             }
-        }
-
+        }*/
     }
 
 }
